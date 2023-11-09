65779f69982772531b29e8d90e05c17f90e0b8c9
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-09 12:48:48 -0300

    Cria variável PORT

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 
 
-app.listen(PORT, () => console.log('Server rodando na porta 3000'));
+app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
