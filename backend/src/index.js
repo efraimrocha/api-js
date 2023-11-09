cc3b4ce93d40997e058439c609bde622d9374c0a
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-09 11:43:20 -0300

    nodemon install

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (requst, response) => response.status(200).send('Index - Servidor rodando'));
+app.get('/tasks', (request, response) => response.status(200).send('Index - Servidor rodando'));
 
 app.listen(3000, () => console.log('Server rodando na porta 3000'));
