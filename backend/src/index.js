313a1d55266ec227d1dce70cf2d331849ae3f7e0
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-09 12:39:13 -0300

    Criar rotas, env, e model

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/tasks', (request, response) => response.status(200).send('Index - Servidor rodando'));
-
-app.listen(3000, () => console.log('Server rodando na porta 3000'));
