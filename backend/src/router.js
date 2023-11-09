0f89e31988ae3ba281132a1428f05197235a1b24
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-09 20:05:44 -0300

    Add tasksControllers e route getAll

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const tasksController = require('./controllers/tasksController')
 
 const router = express.Router();
 
-router.get('/tasks', (req,res) => res.status(200).send('A rota está funcionando!'));
+router.get('/tasks', tasksController.getAll);
 
 module.exports = router;
