7667366d0c839e2757a22027b070231f9fdf86d9
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-10 02:02:40 -0300

    Variaveis de connection

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,10 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable linebreak-style */
 const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+
+console.log(process.env.MYSQL_HOST);
+console.log(process.env.MYSQL_USER);
+console.log(process.env.MYSQL_PASSWORD);
+console.log(process.env.MYSQL_DB);
 
 const connection = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB
+    database: process.env.MYSQL_DB,
 });
 
 module.exports = connection;
