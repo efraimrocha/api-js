2cecc10cd35ced4083e14610a68327d139d595fb
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-10 03:05:11 -0300

    Mudança da forma de seleção da 1ª posição do array

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -3,8 +3,8 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-    const tasks = await connection.execute('SELECT * FROM tasks');
-    return tasks[0];
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
+    return tasks;
 };
 
 module.exports = {
