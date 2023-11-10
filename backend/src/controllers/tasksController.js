530c6d4907494916a79ef5554a084f25ea3ef452
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-10 02:59:20 -0300

    Config eslint

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 // Importa o model tasksModel
 const tasksModel = require('../models/tasksModel');
 
@@ -5,12 +6,12 @@ const tasksModel = require('../models/tasksModel');
 const getAll = async (request, response) => {
 
 
-    const tasks = await tasksModel.getAll();
+	const tasks = await tasksModel.getAll();
 
-    return response.status(200).json(tasks);
+	return response.status(200).json(tasks);
 };
 
 // Exporta o conteúkdo do tasksController
 module.exports = {
-    getAll
+	getAll
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
 const app = require('./app');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-
-
 
 app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
