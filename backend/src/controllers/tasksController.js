7e761ae6baf1f124e7b9ead1a5a92197f1c70fc1
Efraim Rocha <67542881+efraimrocha@users.noreply.github.com>
2023-11-10 22:06:19 -0300

    Aplicando limpesa de código.

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -4,10 +4,7 @@ const tasksModel = require('../models/tasksModel');
 
 
 const getAll = async (request, response) => {
-
-
 	const tasks = await tasksModel.getAll();
-
 	return response.status(200).json(tasks);
 };
 
